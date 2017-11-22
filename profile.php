ceb9d800b9bbc8c0b6369b9ebc59ca15bcd2596f
Janaza <jens.grt@gmail.com>
2017-11-23 00:52:01 +0100

    Corrected src links

--- profile.php
+++ profile.php
@@ -7,36 +7,36 @@ $address = $_SESSION['Address']; ?>
 <!DOCTYPE html>
 <html lang="sv">
 	<head>
-		<link rel="stylesheet" href="main.css">
-		<link rel="stylesheet" href="profile.css">
-		<link rel="stylesheet" href="login-register.css">
+		<link rel="stylesheet" href="css/main.css">
+		<link rel="stylesheet" href="css/profile.css">
+		<link rel="stylesheet" href="css/login-register.css">
 		<meta charset="utf-8">
 		<title>profile</title>
 	</head>
 	<body>
 	<div class="nav">
     <div class="dropdown">
-      <span><a href="home.php"><img src="logo.svg" height="50px" width="50px"/></a></span>
+      <span><a href="home.php"><img src="images/logo.svg" height="50px" width="50px"/></a></span>
       <div class="dropdown-content">
         <a href="login.php">Login</a><br>
         <a href="shop.php">Shop</a>
       </div>
     </div>
     <div class="dropdown2">
-			<span><a href="#"><img src="profile.png" height="50px" width="50px"/></a></span>
+			<span><a href="#"><img src="images/profile.png" height="50px" width="50px"/></a></span>
 			<div class="dropdown-content2">
 				<a href="#">Profile</a><br>
 				<a href="#">Logout</a>
 			</div>
 		</div>
 		<div class="cart">
-			<a href="#"><img src="basket.ico" height="50px" width="50px"/></a>
+			<a href="#"><img src="images/basket.ico" height="50px" width="50px"/></a>
 		</div>
 	</div>
 		<div class="bread-container">
       <div class="profile-edit">
         <div class="profile-icon">
-					<img src="profile.png" height="100px" width="100px"/>
+					<img src="images/profile.png" height="100px" width="100px"/>
 				</div>
 				<div class="vl">
 				</div>
