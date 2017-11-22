cba813baa527898641dcb6d0bdb41439441eb075
Janaza <jens.grt@gmail.com>
2017-11-22 23:14:58 +0100

    added subfolders

--- register.php
+++ register.php
@@ -83,29 +83,29 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
 <!DOCTYPE html>
 <html lang="sv">
 	<head>
-		<link rel="stylesheet" href="main.css">
-		<link rel="stylesheet" href="login-register.css">
+		<link rel="stylesheet" href="css/main.css">
+		<link rel="stylesheet" href="css/login-register.css">
 		<meta charset="utf-8">
 		<title>test</title>
 	</head>
 	<body>
 	<div class="nav">
 		<div class="dropdown">
-			<span><a href="home.php"><img src="logo.svg" height="50px" width="50px"/></a></span>
+			<span><a href="home.php"><img src="images/logo.svg" height="50px" width="50px"/></a></span>
 			<div class="dropdown-content">
 				<a href="login.php">Login</a><br>
 				<a href="shop.php">Shop</a>
 			</div>
 		</div>
 		<div class="dropdown2">
-			<span><a href="#"><img src="profile.png" height="50px" width="50px"/></a></span>
+			<span><a href="#"><img src="images/profile.png" height="50px" width="50px"/></a></span>
 			<div class="dropdown-content2">
 				<a href="#">Profile</a><br>
 				<a href="#">Logout</a>
 			</div>
 		</div>
 		<div class="cart">
-			<a href="#"><img src="basket.ico" height="50px" width="50px"/></a>
+			<a href="#"><img src="images/basket.ico" height="50px" width="50px"/></a>
 		</div>
 	</div>
 		<div class="bread-container">
--- shop.php
+++ shop.php
@@ -2,34 +2,34 @@
 <!DOCTYPE html>
 <html lang="sv">
 	<head>
-		<link rel="stylesheet" href="main.css">
-		<link rel="stylesheet" href="products.css">
+		<link rel="stylesheet" href="css/main.css">
+		<link rel="stylesheet" href="css/products.css">
 		<meta charset="utf-8">
 		<title>test</title>
 	</head>
 	<body>
 	<div class="nav">
     <div class="dropdown">
-      <span><a href="home.php"><img src="logo.svg" height="50px" width="50px"/></a></span>
+      <span><a href="home.php"><img src="images/logo.svg" height="50px" width="50px"/></a></span>
       <div class="dropdown-content">
         <a href="login.php">Login</a><br>
         <a href="shop.php">Shop</a>
       </div>
     </div>
     <div class="dropdown2">
-			<span><a href="#"><img src="profile.png" height="50px" width="50px"/></a></span>
+			<span><a href="#"><img src="images/profile.png" height="50px" width="50px"/></a></span>
 			<div class="dropdown-content2">
 				<a href="profile.php">Profile</a><br>
 				<a href="#">Logout</a>
 			</div>
 		</div>
 		<div class="cart">
-			<a href="#"><img src="basket.ico" height="50px" width="50px"/></a>
+			<a href="#"><img src="images/basket.ico" height="50px" width="50px"/></a>
 		</div>
 	</div>
 		<div class="bread-container">
 			<figure class="product-container">
-  			<img src="bike3.jpg" />
+  			<img src="bikes/bike3.jpg" />
   				<figcaption>
     			<h1>Giant 1</h1>
     				<p>Giant down-hill bike.</p>
@@ -41,7 +41,7 @@
 			</figure>
 
 			<figure class="product-container">
-  			<img src="bike2.jpeg" />
+  			<img src="bikes/bike2.jpeg" />
   				<figcaption>
     			<h1>Giant 1</h1>
     				<p>Giant down-hill bike.</p>
@@ -53,7 +53,7 @@
 			</figure>
 
 			<figure class="product-container">
-  			<img src="bike3.jpg" />
+  			<img src="bikes/bike3.jpg" />
   				<figcaption>
     			<h1>Giant 1</h1>
     				<p>Giant down-hill bike.</p>
