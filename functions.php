d3f7b372bc85851d538c48e41947949d1df98a0b
MrRelaxaaa <richardhultstrand@gmail.com>
2017-12-10 17:49:51 +0100

    css changes

--- functions.php
+++ functions.php
@@ -259,10 +259,7 @@ function addOrder(){
 }
 
 function getProfile($username, $link){
-  //include 'config.php';
-  //  echo $link;
   $query = "SELECT firstname, lastname, address FROM users WHERE Kundnr = (SELECT Kundnr FROM logins WHERE Username = '" . $username . "')";
-  #test log in : Echo "Logged in as: " .$username2;
   $test = (mysqli_query($link, $query));
 	while($kundnr = mysqli_fetch_assoc($test)){
  		$firstname = $kundnr['firstname'];
